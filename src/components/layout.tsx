f952f155d5abf1b5bb5219d1c017313f2e7af38a
Sirojiddin <ishankhanovsirojiddin@gmail.com>
2024-01-03 11:01:43 +0600

    commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,14 @@
-export default function Layout({ children }:{children:React.ReactNode}) {
+import {ReactNode} from "react";
+
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout({ children }:{children: ReactNode}) {
   return (
+    <>
+    <Header/>
       <main>{children}</main>
+    <Footer/>
+    </>
   )
-}
+} 
